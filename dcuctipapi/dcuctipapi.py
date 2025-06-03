8e6790bfb1536ff0c9b37a965fda72bb7cc7a404
Christopher <37674669+cdrphog@users.noreply.github.com>
2025-06-03 17:07:52 -0400

    Saving initial file structure

---
+++ dcuctipapi/dcuctipapi.py
@@ -0,0 +1,19 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Licensed under the MIT License. See LICENSE in the project root for license information.
+
+Microsoft Digital Crimes Unit 
+Cyber Threat Intelligence Program 
+
+dcuctipapi.py demonstrates how to connect to the CTIP API to download and process DCU CTIP data
+  - Supported datasets: CTIP Infected, CTIP C2
+
+Install required libraries:
+     run: pip install -r requirements.txt
+  or run:   python3 -m pip install -r requirements.txt
+"""
+
+#
+# Coming soon...
+#
